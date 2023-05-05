cc3e8ad5be103b92cb81a23ae131a7971e75484b
Henrique Costa <henrysilva14@gmail.com>
2023-05-04 21:08:40 -0300

    add libs cors and express

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,16 @@
-class AppServer {
-  private app: string
+import express from 'express'
+import 'express-async-errors'
+import cors from 'cors'
 
-  constructor(info: string) {
-    this.app = info ?? 'Olá dev'
-  }
-}
+const app = express()
+app.use(cors())
+
+app.use(express.json())
+
+app.get('/', (request: Request, response: Response) => {
+  return response.json({ message: 'Olá dev!' })
+})
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000')
+})
