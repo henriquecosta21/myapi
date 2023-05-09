a11d3e645deab7d783b0fa74ba83fe6a8b2130d8
Henrique Costa <henrysilva14@gmail.com>
2023-05-09 08:02:28 -0300

    add catch to get a possible connection error and show it in console

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,8 +3,13 @@ import 'reflect-metadata'
 import { app } from './app'
 import { dataSource } from '../typeorm'
 
-dataSource.initialize().then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log(`Server started on port ${process.env.PORT}`)
+dataSource
+  .initialize()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server started on port ${process.env.PORT}`)
+    })
   })
-})
+  .catch(err => {
+    console.error('Error during dataSource initialization!', err)
+  })
